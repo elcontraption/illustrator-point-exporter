4c7d023f21cde4bc82da47750f5d8daa1e26edf1
Darin Reid <darinreid@gmail.com>
2016-01-06 13:21:28 -0800

    Point anchor coordinates are now calculated relative to the artboard instead of the document. Closes #1.

--- dist/exporter.js
+++ dist/exporter.js
@@ -54,6 +54,7 @@
 	    if (app.documents.length === 0) return;
 
 	    this.document = app.activeDocument;
+	    this.getArtboardInfo();
 
 	    this.readLayers();
 	    this.writeFile();
@@ -61,9 +62,34 @@
 
 	Exporter.prototype = {
 	    document: {},
+	    artboard: { x: 0, y: 0, w: 0, h: 0 },
 	    output: {
 	        layers: []
 	    }
+	};
+
+	/**
+	 * Get information about the artboard
+	 */
+	Exporter.prototype.getArtboardInfo = function () {
+	    var artboardIndex = this.document.artboards.getActiveArtboardIndex();
+	    var artboard = this.document.artboards[artboardIndex];
+
+	    this.artboard.x = artboard.artboardRect[0];
+	    this.artboard.y = artboard.artboardRect[1];
+
+	    // Width is based on artboard x value
+	    this.artboard.w = Math.abs(artboard.artboardRect[2] - this.artboard.x);
+
+	    // Height is based on artboard y value
+	    this.artboard.h = Math.abs(artboard.artboardRect[3] - this.artboard.y);
+
+	    this.log('artboard:');
+	    this.log(this.artboard.x);
+	    this.log(this.artboard.y);
+	    this.log(this.artboard.w);
+	    this.log(this.artboard.h);
+	    this.log(' ');
 	};
 
 	/**
@@ -77,11 +103,8 @@
 	    for (i = 0; i < layerCount; i++) {
 	        layer = this.document.layers[i];
 
-	        // Skip if hidden
-	        if (layer.hidden) continue;
-
-	        // Skip if no paths
-	        if (layer.pathItems.length === 0) continue;
+	        // Skip if locked, hidden, or no paths
+	        if (layer.hidden || layer.locked || layer.pathItems === 0) continue;
 
 	        this.output.layers.push({
 	            name: layer.name,
@@ -122,19 +145,36 @@
 	Exporter.prototype.readPathPoints = function (path) {
 	    var pointCount = path.pathPoints.length;
 	    var points = [];
-	    var anchorX;
-	    var anchorY;
 	    var i;
 
 	    if (path.hidden) return;
 
 	    for (i = 0; i < pointCount; i++) {
-	        anchorX = path.pathPoints[i].anchor[0];
-	        anchorY = this.document.artboards[0].artboardRect[1] - path.pathPoints[i].anchor[1];
-	        points.push([anchorX, anchorY]);
+	        points.push(this.getPointAnchorSet(path.pathPoints[i]));
 	    }
 
 	    return points;
+	};
+
+	/**
+	 * Adjusts anchor coordinates relative to the artboard position
+	 * within the document. Returns an anchor array.
+	 *
+	 * @param  {object} point
+	 * @return {array}
+	 */
+	Exporter.prototype.getPointAnchorSet = function (point) {
+	    var anchorX = point.anchor[0];
+	    var anchorY = point.anchor[1];
+
+	    anchorX = Math.abs(anchorX - this.artboard.x);
+	    anchorY = Math.abs(anchorY - this.artboard.y);
+
+	    this.log('---');
+	    this.log(anchorY);
+	    this.log('---');
+
+	    return [anchorX, anchorY];
 	};
 
 	/**
--- src/exporter.js
+++ src/exporter.js
@@ -6,6 +6,7 @@ var Exporter = function () {
     if (app.documents.length === 0) return;
 
     this.document = app.activeDocument;
+    this.getArtboardInfo();
 
     this.readLayers();
     this.writeFile();
@@ -13,9 +14,27 @@ var Exporter = function () {
 
 Exporter.prototype = {
     document: {},
+    artboard: { x: 0, y: 0, w: 0, h: 0 },
     output: {
         layers: []
     }
+};
+
+/**
+ * Get information about the artboard
+ */
+Exporter.prototype.getArtboardInfo = function () {
+    var artboardIndex = this.document.artboards.getActiveArtboardIndex();
+    var artboard = this.document.artboards[artboardIndex];
+
+    this.artboard.x = artboard.artboardRect[0];
+    this.artboard.y = artboard.artboardRect[1];
+
+    // Width is based on artboard x value
+    this.artboard.w = Math.abs(artboard.artboardRect[2] - this.artboard.x);
+
+    // Height is based on artboard y value
+    this.artboard.h = Math.abs(artboard.artboardRect[3] - this.artboard.y);
 };
 
 /**
@@ -29,11 +48,8 @@ Exporter.prototype.readLayers = function () {
     for (i = 0; i < layerCount; i++) {
         layer = this.document.layers[i];
 
-        // Skip if hidden
-        if (layer.hidden) continue;
-
-        // Skip if no paths
-        if (layer.pathItems.length === 0) continue;
+        // Skip if locked, hidden, or no paths
+        if (layer.hidden || layer.locked || layer.pathItems === 0) continue;
 
         this.output.layers.push({
             name: layer.name,
@@ -74,19 +90,32 @@ Exporter.prototype.readLayerPaths = function (layer) {
 Exporter.prototype.readPathPoints = function (path) {
     var pointCount = path.pathPoints.length;
     var points = [];
-    var anchorX;
-    var anchorY;
     var i;
 
     if (path.hidden) return;
 
     for (i = 0; i < pointCount; i++) {
-        anchorX = path.pathPoints[i].anchor[0];
-        anchorY = this.document.artboards[0].artboardRect[1] - path.pathPoints[i].anchor[1];
-        points.push([anchorX, anchorY]);
+        points.push(this.getPointAnchorSet(path.pathPoints[i]));
     }
 
     return points;
+};
+
+/**
+ * Adjusts anchor coordinates relative to the artboard position
+ * within the document. Returns an anchor array.
+ *
+ * @param  {object} point
+ * @return {array}
+ */
+Exporter.prototype.getPointAnchorSet = function (point) {
+    var anchorX = point.anchor[0];
+    var anchorY = point.anchor[1];
+
+    anchorX = Math.abs(anchorX - this.artboard.x);
+    anchorY = Math.abs(anchorY - this.artboard.y);
+
+    return [anchorX, anchorY];
 };
 
 /**
